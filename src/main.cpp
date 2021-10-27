31967eab1ecb9e5437cca6cc1ae71c42e97846b1
Yargachin-wye <henryhxv@gmail.com>
2021-10-28 02:16:53 +0700

    1 commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,571 +1,248 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "RBtree.h"
 
 using namespace std;
 
-struct Node
+// Define the character size
+#define CHAR_SIZE 128
+
+// A class to store a node
+class Trie
 {
-	int data;	  // the key
-	Node *parent;
-	Node *left;
-	Node *right;
-	int color;	  // 1 -> Red, 0 -> Black
-};
-
-typedef Node *NodePtr;
-
-class RBTree
-{
-private:
-	NodePtr root;
-	NodePtr TNULL;
-
-	// initializes the nodes with appropirate values
-	// all the pointers are set to point to the null pointer
-	void initializeNULLNode(NodePtr node, NodePtr parent)
-	{
-		node->data = 0;
-		node->parent = parent;
-		node->left = nullptr;
-		node->right = nullptr;
-		node->color = 0;
-	}
-
-	void preOrderHelper(NodePtr node)
-	{
-		if (node != TNULL)
-		{
-			cout << node->data << " ";
-			preOrderHelper(node->left);
-			preOrderHelper(node->right);
-		}
-	}
-
-	void inOrderHelper(NodePtr node)
-	{
-		if (node != TNULL)
-		{
-			inOrderHelper(node->left);
-			cout << node->data << " ";
-			inOrderHelper(node->right);
-		}
-	}
-
-	void postOrderHelper(NodePtr node)
-	{
-		if (node != TNULL)
-		{
-			postOrderHelper(node->left);
-			postOrderHelper(node->right);
-			cout << node->data << " ";
-		}
-	}
-
-	NodePtr searchTreeHelper(NodePtr node, int key)
-	{
-		if (node == TNULL || key == node->data)
-		{
-			return node;
-		}
-
-		if (key < node->data)
-		{
-			return searchTreeHelper(node->left, key);
-		}
-		return searchTreeHelper(node->right, key);
-	}
-
-	void fixDelete(NodePtr x)
-	{
-		NodePtr s;
-		while (x != root && x->color == 0)
-		{
-			if (x == x->parent->left)
-			{
-				s = x->parent->right;
-				if (s->color == 1)
-				{
-					// case 3.1
-					s->color = 0;
-					x->parent->color = 1;
-					leftRotate(x->parent);
-					s = x->parent->right;
-				}
-
-				if (s->left->color == 0 && s->right->color == 0)
-				{
-					// case 3.2
-					s->color = 1;
-					x = x->parent;
-				}
-				else
-				{
-					if (s->right->color == 0)
-					{
-						// case 3.3
-						s->left->color = 0;
-						s->color = 1;
-						rightRotate(s);
-						s = x->parent->right;
-					}
-
-					// case 3.4
-					s->color = x->parent->color;
-					x->parent->color = 0;
-					s->right->color = 0;
-					leftRotate(x->parent);
-					x = root;
-				}
-			}
-			else
-			{
-				s = x->parent->left;
-				if (s->color == 1)
-				{
-					// case 3.1
-					s->color = 0;
-					x->parent->color = 1;
-					rightRotate(x->parent);
-					s = x->parent->left;
-				}
-
-				if (s->right->color == 0 && s->right->color == 0)
-				{
-					// case 3.2
-					s->color = 1;
-					x = x->parent;
-				}
-				else
-				{
-					if (s->left->color == 0)
-					{
-						// case 3.3
-						s->right->color = 0;
-						s->color = 1;
-						leftRotate(s);
-						s = x->parent->left;
-					}
-
-					// case 3.4
-					s->color = x->parent->color;
-					x->parent->color = 0;
-					s->left->color = 0;
-					rightRotate(x->parent);
-					x = root;
-				}
-			}
-		}
-		x->color = 0;
-	}
-
-	void rbTransplant(NodePtr u, NodePtr v)
-	{
-		if (u->parent == nullptr)
-		{
-			root = v;
-		}
-		else if (u == u->parent->left)
-		{
-			u->parent->left = v;
-		}
-		else
-		{
-			u->parent->right = v;
-		}
-		v->parent = u->parent;
-	}
-
-	void deleteNodeHelper(NodePtr node, int key)
-	{
-		NodePtr z = TNULL;
-		NodePtr x, y;
-		while (node != TNULL)
-		{
-			if (node->data == key)
-			{
-				z = node;
-			}
-
-			if (node->data <= key)
-			{
-				node = node->right;
-			}
-			else
-			{
-				node = node->left;
-			}
-		}
-
-		if (z == TNULL)
-		{
-			cout << "Couldn't find key in the tree" << endl;
-			return;
-		}
-
-		y = z;
-		int y_original_color = y->color;
-		if (z->left == TNULL)
-		{
-			x = z->right;
-			rbTransplant(z, z->right);
-		}
-		else if (z->right == TNULL)
-		{
-			x = z->left;
-			rbTransplant(z, z->left);
-		}
-		else
-		{
-			y = minimum(z->right);
-			y_original_color = y->color;
-			x = y->right;
-			if (y->parent == z)
-			{
-				x->parent = y;
-			}
-			else
-			{
-				rbTransplant(y, y->right);
-				y->right = z->right;
-				y->right->parent = y;
-			}
-
-			rbTransplant(z, y);
-			y->left = z->left;
-			y->left->parent = y;
-			y->color = z->color;
-		}
-		delete z;
-		if (y_original_color == 0)
-		{
-			fixDelete(x);
-		}
-	}
-
-	void fixInsert(NodePtr k)
-	{
-		NodePtr u; // uncle
-		while (k->parent->color == 1)
-		{
-			if (k->parent == k->parent->parent->right)
-			{
-				u = k->parent->parent->left;
-				if (u->color == 1)
-				{
-					// case 3.1
-					u->color = 0;
-					k->parent->color = 0;
-					k->parent->parent->color = 1;
-					k = k->parent->parent;
-				}
-				else
-				{
-					if (k == k->parent->left)
-					{
-						// case 3.2.2
-						k = k->parent;
-						rightRotate(k);
-					}
-					// case 3.2.1
-					k->parent->color = 0;
-					k->parent->parent->color = 1;
-					leftRotate(k->parent->parent);
-				}
-			}
-			else
-			{
-				u = k->parent->parent->right;
-
-				if (u->color == 1)
-				{
-					// mirror case 3.1
-					u->color = 0;
-					k->parent->color = 0;
-					k->parent->parent->color = 1;
-					k = k->parent->parent;
-				}
-				else
-				{
-					if (k == k->parent->right)
-					{
-						// mirror case 3.2.2
-						k = k->parent;
-						leftRotate(k);
-					}
-					// mirror case 3.2.1
-					k->parent->color = 0;
-					k->parent->parent->color = 1;
-					rightRotate(k->parent->parent);
-				}
-			}
-			if (k == root)
-			{
-				break;
-			}
-		}
-		root->color = 0;
-	}
-
-	void printHelper(NodePtr root, string indent, bool last)
-	{
-		if (root != TNULL)
-		{
-			cout << indent;
-			if (last)
-			{
-				cout << "R----";
-				indent += "     ";
-			}
-			else
-			{
-				cout << "L----";
-				indent += "|    ";
-			}
-
-			string sColor = root->color ? "RED" : "BLACK";
-			cout << root->data << "(" << sColor << ")" << endl;
-			printHelper(root->left, indent, false);
-			printHelper(root->right, indent, true);
-		}
-		// cout<<root->left->data<<endl;
-	}
-
 public:
-	RBTree()
-	{
-		TNULL = new Node;
-		TNULL->color = 0;
-		TNULL->left = nullptr;
-		TNULL->right = nullptr;
-		root = TNULL;
-	}
+    bool isLeaf;
+    Trie *character[CHAR_SIZE];
 
-	// Pre-Order traversal
-	// Node->Left Subtree->Right Subtree
-	void preorder()
-	{
-		preOrderHelper(this->root);
-	}
+    // Constructor
+    Trie()
+    {
+        this->isLeaf = false;
 
-	// In-Order traversal
-	// Left Subtree -> Node -> Right Subtree
-	void inorder()
-	{
-		inOrderHelper(this->root);
-	}
+        for (int i = 0; i < CHAR_SIZE; i++)
+        {
+            this->character[i] = nullptr;
+        }
+    }
 
-	// Post-Order traversal
-	// Left Subtree -> Right Subtree -> Node
-	void postorder()
-	{
-		postOrderHelper(this->root);
-	}
-
-	// return the corresponding node for the key
-	NodePtr searchTree(int k)
-	{
-		return searchTreeHelper(this->root, k);
-	}
-
-	NodePtr minimum(NodePtr node)
-	{
-		while (node->left != TNULL)
-		{
-			node = node->left;
-		}
-		return node;
-	}
-
-	NodePtr maximum(NodePtr node)
-	{
-		while (node->right != TNULL)
-		{
-			node = node->right;
-		}
-		return node;
-	}
-
-	// find the successor of a given node
-	NodePtr successor(NodePtr x)
-	{
-		// if the right subtree is not null,
-		// the successor is the leftmost node in the
-		// right subtree
-		if (x->right != TNULL)
-		{
-			return minimum(x->right);
-		}
-
-		// else it is the lowest ancestor of x whose
-		// left child is also an ancestor of x.
-		NodePtr y = x->parent;
-		while (y != TNULL && x == y->right)
-		{
-			x = y;
-			y = y->parent;
-		}
-		return y;
-	}
-
-	// find the predecessor of a given node
-	NodePtr predecessor(NodePtr x)
-	{
-		// if the left subtree is not null,
-		// the predecessor is the rightmost node in the
-		// left subtree
-		if (x->left != TNULL)
-		{
-			return maximum(x->left);
-		}
-
-		NodePtr y = x->parent;
-		while (y != TNULL && x == y->left)
-		{
-			x = y;
-			y = y->parent;
-		}
-
-		return y;
-	}
-
-	// rotate left at node x
-	void leftRotate(NodePtr x)
-	{
-		NodePtr y = x->right;
-		x->right = y->left;
-		if (y->left != TNULL)
-		{
-			y->left->parent = x;
-		}
-		y->parent = x->parent;
-		if (x->parent == nullptr)
-		{
-			this->root = y;
-		}
-		else if (x == x->parent->left)
-		{
-			x->parent->left = y;
-		}
-		else
-		{
-			x->parent->right = y;
-		}
-		y->left = x;
-		x->parent = y;
-	}
-
-	// rotate right at node x
-	void rightRotate(NodePtr x)
-	{
-		NodePtr y = x->left;
-		x->left = y->right;
-		if (y->right != TNULL)
-		{
-			y->right->parent = x;
-		}
-		y->parent = x->parent;
-		if (x->parent == nullptr)
-		{
-			this->root = y;
-		}
-		else if (x == x->parent->right)
-		{
-			x->parent->right = y;
-		}
-		else
-		{
-			x->parent->left = y;
-		}
-		y->right = x;
-		x->parent = y;
-	}
-
-	void insert(int key)
-	{
-		
-		NodePtr node = new Node;
-		node->parent = nullptr;
-		node->data = key;
-		node->left = TNULL;
-		node->right = TNULL;
-		node->color = 1; // new node must be red
-
-		NodePtr y = nullptr;
-		NodePtr x = this->root;
-
-		while (x != TNULL)
-		{
-			y = x;
-			if (node->data < x->data)
-			{
-				x = x->left;
-			}
-			else
-			{
-				x = x->right;
-			}
-		}
-
-		// y is parent of x
-		node->parent = y;
-		if (y == nullptr)
-		{
-			root = node;
-		}
-		else if (node->data < y->data)
-		{
-			y->left = node;
-		}
-		else
-		{
-			y->right = node;
-		}
-
-		if (node->parent == nullptr)//--
-		{
-			node->color = 0;
-			return;
-		}
-
-		if (node->parent->parent == nullptr)//--
-		{
-			return;
-		}
-
-		fixInsert(node);
-	}
-
-	NodePtr getRoot()
-	{
-		return this->root;
-	}
-
-	void deleteNode(int data)
-	{
-		deleteNodeHelper(this->root, data);
-	}
-
-	void prettyPrint()
-	{
-		if (root)
-		{
-			printHelper(this->root, "", true);
-		}
-	}
+    void insert(string);
+    bool deletion(Trie *&, string);
+    bool search(string);
+    bool haveChildren(Trie const *);
 };
+
+// insert a key into a Trie
+void Trie::insert(string key)
+{
+    // start from the root node
+    Trie *curr = this;
+    for (int i = 0; i < key.length(); i++)
+    {
+        // create a new node if the path doesn't exist
+        if (curr->character[key[i]] == nullptr)
+        {
+            curr->character[key[i]] = new Trie();
+        }
+
+        // go to the next node
+        curr = curr->character[key[i]];
+    }
+
+    // mark the current node as a leaf
+    curr->isLeaf = true;
+}
+
+// search a key, true if the key is found; otherwise false
+bool Trie::search(string key)
+{
+    //false if Trie is empty
+    if (this == nullptr)
+    {
+        return false;
+    }
+
+    Trie *curr = this;
+    for (int i = 0; i < key.length(); i++)
+    {
+        // go to the next node
+        curr = curr->character[key[i]];
+
+        // if the string is invalid (reached end of a path in the Trie)
+        if (curr == nullptr)
+        {
+            return false;
+        }
+    }
+
+    // return true if the current node is a leaf and the
+    // end of the string is reached
+    return curr->isLeaf;
+}
+
+// true if a node has any children
+bool Trie::haveChildren(Trie const *curr)
+{
+    for (int i = 0; i < CHAR_SIZE; i++)
+    {
+        if (curr->character[i])
+        {
+            return true; // child found
+        }
+    }
+
+    return false;
+}
+
+// Recursive function to delete a key in the Trie
+bool Trie::deletion(Trie *&curr, string key)
+{
+    // return if Trie is empty
+    if (curr == nullptr)
+    {
+        return false;
+    }
+
+    // if the end of the key is not reached
+    if (key.length())
+    {
+        // recur for the node corresponding to the next character in the key
+        // and if it returns true, delete the current node (if it is non-leaf)
+
+        if (curr != nullptr &&
+            curr->character[key[0]] != nullptr &&
+            deletion(curr->character[key[0]], key.substr(1)) &&
+            curr->isLeaf == false)
+        {
+            if (!haveChildren(curr))
+            {
+                delete curr;
+                curr = nullptr;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    // if the end of the key is reached
+    if (key.length() == 0 && curr->isLeaf)
+    {
+        // if the current node is a leaf node and doesn't have any children
+        if (!haveChildren(curr))
+        {
+            // delete the current node
+            delete curr;
+            curr = nullptr;
+
+            // delete the non-leaf parent nodes
+            return true;
+        }
+
+        // if the current node is a leaf node and has children
+        else
+        {
+            // mark the current node as a non-leaf node (DON'T DELETE IT)
+            curr->isLeaf = false;
+
+            // don't delete its parent nodes
+            return false;
+        }
+    }
+
+    return false;
+}
+
+/*int main()
+{
+    Trie* head = new Trie();
+ 
+    head->insert("hello");
+    cout << head->search("hello") << " ";      // print 1
+ 
+    head->insert("helloworld");
+    cout << head->search("helloworld") << " "; // print 1
+ 
+    cout << head->search("helll") << " ";      // print 0 (Not found)
+ 
+    head->insert("hell");
+    cout << head->search("hell") << " ";       // print 1
+ 
+    head->insert("h");
+    cout << head->search("h");                 // print 1
+ 
+    cout << endl;
+ 
+    head->deletion(head, "hello");
+    cout << head->search("hello") << " ";      // print 0
+ 
+    cout << head->search("helloworld") << " "; // print 1
+    cout << head->search("hell");              // print 1
+ 
+    cout << endl;
+ 
+    head->deletion(head, "h");
+    cout << head->search("h") << " ";          // print 0
+    cout << head->search("hell") << " ";       // print 1
+    cout << head->search("helloworld");        // print 1
+ 
+    cout << endl;
+ 
+    head->deletion(head, "helloworld");
+    cout << head->search("helloworld") << " "; // print 0
+    cout << head->search("hell") << " ";       // print 1
+ 
+    head->deletion(head, "hell");
+    cout << head->search("hell");              // print 0
+ 
+    cout << endl;
+ 
+    if (head == nullptr) {
+        cout << "Trie empty!!\n";              // Trie is empty now
+    }
+ 
+    cout << head->search("hell");              // print 0
+ 
+    return 0;
+}*/
 
 int main()
 {
-	RBTree rbtree;
-	rbtree.insert(8);
-	rbtree.insert(18);
-	rbtree.insert(5);
-	rbtree.insert(15);
-	rbtree.insert(17);
-	rbtree.insert(25);
-	rbtree.insert(40);
-	rbtree.insert(80);
+    ifstream fin;
+    fin.open("./text.txt");
+    vector<string> dict;
 
-	rbtree.deleteNode(25);
-	rbtree.prettyPrint();
-	return 0;
+    if (fin.is_open())
+    {
+        cout << "File Opened successfully" << endl;
+        string word;
+        while (getline(fin, word, '\0'))
+        {
+            dict.push_back(word); //putting word into vec
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "File could not be opened." << endl;
+        return 0;
+    }
+    fin.close();
+    
+    cout << dict[0]<< endl;
+
+    RBTree rbtree;
+
+    Trie* head = new Trie();
+
+    /*for (int i = 0; i < 2; i++)
+    {
+        head->insert(dict[i]);
+    }*/
+
+    rbtree.prettyPrint();
+    return 0;
 }
